6afd45cd10e5c95eeac8f96a2b120e600a0e1595
Michal Och <michal.och@pl.abb.com>
2020-07-20 20:05:47 +0200

    ItemController
    New controller

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext contex)
+        {
+            _context = contex;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
